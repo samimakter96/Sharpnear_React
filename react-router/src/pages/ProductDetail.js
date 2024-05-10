5216e7b0254165cc213068c16f9a1f7ac18f6d78
samimakter96 <samimakter9641@gmail.com>
2024-05-10 18:34:15 +0530

    learning react router

---
+++ react-router/src/pages/ProductDetail.js
@@ -0,0 +1,15 @@
+import {useParams} from 'react-router-dom'
+
+const ProductDetails = () => {
+
+  const params = useParams()
+
+  console.log(params.productId);
+
+  return <section>
+    <h1>Product Detail</h1>
+    <p>{params.productId}</p>
+  </section>
+}
+
+export default ProductDetails;
