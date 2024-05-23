1f2a13e2d841c07ea11f3bdc629c69476e296da8
samimakter96 <samimakter9641@gmail.com>
2024-05-23 09:20:18 +0530

    Subscribing and Dispatching Actions

---
+++ Redux-course/Redux-project/src/components/Counter.js
@@ -0,0 +1,41 @@
+import { useSelector, useDispatch } from 'react-redux';
+import classes from './Counter.module.css';
+
+const Counter = () => {
+  const dispatch = useDispatch()
+  const counter = useSelector(state => state.counter)
+
+  const incrementHandler = () => {
+    dispatch({type: 'increment'})
+  }
+
+  const decrementHandler = () => {
+    dispatch({type: 'decrement'})
+  }
+
+  const incrementBy5Handler = () => {
+    dispatch({ type: 'incrementBy5', payload: 5 });
+  }
+
+  const decrementBy5Handler = () => {
+    dispatch({ type: 'decrementBy5', payload: 5 });
+  };
+
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={incrementBy5Handler}>IncrementBy5</button>
+        <button onClick={decrementBy5Handler}>DecrementBy5</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
---
+++ Redux-course/Redux-project/src/store/index.js
@@ -0,0 +1,32 @@
+import {createStore} from 'redux'
+
+const counterReducer = (state = {counter: 0}, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1
+    }
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1
+    }
+  }
+
+  if (action.type === 'incrementBy5') {
+    return {
+      counter: state.counter + action.payload
+    }
+  }
+  if (action.type === 'decrementBy5') {
+    return {
+      counter: state.counter - action.payload
+    }
+  }
+
+  return state
+}
+
+const store = createStore(counterReducer)
+
+export default store;
